525f2be9890f0b6941f650cff7061280058a41ae
khushbu <sonawanekhushbu@gmail.com>
2021-12-15 01:38:37 +0530

    PraticalAss7

---
+++ src/Sdata.js
@@ -0,0 +1,48 @@
+const sdata  = [
+    {
+        id : 1,
+        imgsrc:"https://im.indiatimes.in/content/2020/Aug/money-heist_5f4617cae7388.jpeg?w=725&h=407",
+        title:"A Netflix Original Series",
+        sname : 'MONEY-HEIST',
+        link:'https://www.netflix.com/in/title/80192098'
+    },
+    {
+        id : 2,
+        imgsrc:"https://wallpapercave.com/wp/wp1917154.jpg",
+        title:"A Netflix Original Series",
+        sname : 'STRAGER THINGS',
+        link :'https://www.netflix.com/in/title/80057281'
+    },
+
+    {
+        id : 3,
+        imgsrc :"https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTcitm1SWnM5sdeNj2TKEw8g2XlmNmTlhhXzVmzbOQZI1pNH0M5",
+        title :"A Netflix Original Series",
+        sname : 'BLACK MIRROR',
+        link :'https://www.netflix.com/in/title/70264888'
+    },  
+
+    {
+        id : 4,
+        imgsrc :"https://m.media-amazon.com/images/M/MV5BNGQxMDcyOTEtZWZkNi00NzgwLWEzNjQtZmFhMGY5ZGRlMTdiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg",
+        title :"A Netflix Original Series",
+        sname : 'EXTRA CARICURAM',
+        link :'https://www.netflix.com/in/title/80990668?source=35'
+    },  
+    {
+        id : 5,
+        imgsrc:"https://m.media-amazon.com/images/M/MV5BYTFlYWRhYjEtNDU3MC00ZTRiLTlmY2MtNGViODJlNDczNTA4XkEyXkFqcGdeQXVyNjY1MTg4Mzc@._V1_.jpg",
+        title:"A Netflix Original Series",
+        sname : 'NEVER HAVE I EVER',
+        link :'https://www.netflix.com/in/title/80179190'
+    },
+    {
+        id : 6,
+        imgsrc:"https://wallpapercave.com/wp/wp4056410.jpg",
+        title:"A Netflix Original Series",
+        sname : 'DARK',
+        link:'https://www.netflix.com/in/title/80100172'
+    },
+]
+
+export default sdata;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import  ReactDOM  from 'react-dom';
+import Card from './Cards';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import sdata from './Sdata';
+
+function ncard(val){
+return(
+  <Card imgsrc= {val.imgsrc}
+  title= {val.title}
+  sname = {val.sname}
+  link= {val.link}
+  />
+);
+}
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  <>
+<h1 className='heading_style'>List Of Top 5 Netflix Series</h1>
+  {/* custom attribute */}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+{sdata.map(ncard)}; 
+
+ </>,
+
+  document.getElementById("root")
+);
